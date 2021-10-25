ecb35a942069c7bc88e9d4eba67062ab9744ff64
Boisababy Clips <bob@fleming.nowhere>
2021-10-25 21:43:24 +0100

    Randomly fetch a book for ny times

--- src/routes/books/random.ts
+++ src/routes/books/random.ts
@@ -1,10 +1,37 @@
 import type { RequestHandler } from '@sveltejs/kit';
 
 export const get: RequestHandler = async () => {
+  const api =
+    'https://api.nytimes.com/svc/books/v3/lists/best-sellers/history.json?api-key=Rj71AIlkian8S4hL8DLzUKLRHPBPZSpW';
+
+  const numberOfBooks =
+    (await fetch(`${api}&offset=0`)
+      .then((res) => res.json())
+      .then((res) => res.num_results)) || 30000;
+
+  console.log(numberOfBooks);
+
+  const randomBookNumber = Math.round(Math.random() * numberOfBooks);
+  const randomOffset = randomBookNumber - (randomBookNumber % 20);
+
+  console.log('Fetching page', randomOffset);
+
+  const books = await fetch(`${api}&offset=${randomOffset}`)
+    .then((res) => res.json())
+    .then((res) => res.results);
+
+  const randomBookInList = Math.round(Math.random() * (books.length - 1));
+  console.log('Using book', randomBookInList);
+  const book = books[randomBookInList];
+
   return {
     status: 200,
     body: JSON.stringify({
       hello: 'world',
+      ...book,
     }),
+    headers: {
+      'Cache-Control': 'public, max-age=5',
+    },
   };
 };
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,4 +1,34 @@
-<h1>Welcome to SvelteKit</h1>
-<p>
-  Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation
-</p>
+<script lang="ts" context="module">
+  import type { Load } from '@sveltejs/kit';
+
+  export const load: Load = async ({ fetch }) => {
+    const res = await fetch('/books/random');
+
+    if (res.ok) {
+      return {
+        props: {
+          book: await res.json(),
+        },
+      };
+    }
+
+    return {
+      status: res.status,
+      error: new Error(`Could not load a random book`),
+    };
+  };
+</script>
+
+<script lang="ts">
+  export let book;
+</script>
+
+<h2>Your random book is</h2>
+<h3>{book.title}</h3>
+<p>{book.author}</p>
+<a
+  target="#"
+  href={`https://www.goodreads.com/search?q=${encodeURIComponent(
+    `${book.title} ${book.author}`
+  )}&search_type=books`}>Goodreads</a
+>
