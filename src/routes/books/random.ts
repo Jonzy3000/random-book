dbf3b12bcd528bec8e0c23ce4a737f47b1a8f0fc
Matt Jones <matt@boclips.com>
2021-10-27 21:59:39 +0100

    Use openlibrary api to enrich book from nytimes

--- src/routes/books/random.ts
+++ src/routes/books/random.ts
@@ -1,11 +1,11 @@
 import type { RequestHandler } from '@sveltejs/kit';
 
 export const get: RequestHandler = async () => {
-  const api =
+  const nyBestSellerApi =
     'https://api.nytimes.com/svc/books/v3/lists/best-sellers/history.json?api-key=Rj71AIlkian8S4hL8DLzUKLRHPBPZSpW';
 
   const numberOfBooks =
-    (await fetch(`${api}&offset=0`)
+    (await fetch(`${nyBestSellerApi}&offset=0`)
       .then((res) => res.json())
       .then((res) => res.num_results)) || 30000;
 
@@ -16,19 +16,23 @@ export const get: RequestHandler = async () => {
 
   console.log('Fetching page', randomOffset);
 
-  const books = await fetch(`${api}&offset=${randomOffset}`)
+  const books = await fetch(`${nyBestSellerApi}&offset=${randomOffset}`)
     .then((res) => res.json())
     .then((res) => res.results);
 
   const randomBookInList = Math.round(Math.random() * (books.length - 1));
   console.log('Using book', randomBookInList);
-  const book = books[randomBookInList];
+  const nyBook = books[randomBookInList];
+
+  const book = await fetch(
+    `https://openlibrary.org/api/books?bibkeys=ISBN:${nyBook.isbns[0].isbn13}&format=json&jscmd=data`
+  ).then((res) => res.json());
 
   return {
     status: 200,
     body: JSON.stringify({
       hello: 'world',
-      ...book,
+      ...book[`ISBN:${nyBook.isbns[0].isbn13}`],
     }),
     headers: {
       'Cache-Control': 'public, max-age=5',
